d5c2bc5e255f80e3df752dec38151c84019c8dbe
seanbaraka <seanbaraka@gmail.com>
2023-06-12 22:27:06 +0300

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app.vue',
+    './pages/**/*.vue',
+    './components/**/*.vue',
+    './layouts/**/*.vue'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
