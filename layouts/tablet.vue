9ec33406bf017415f38719396bb6f1c0d137f136
seanbaraka <seanbaraka@gmail.com>
2023-06-12 22:27:42 +0300

    feat: tablet layout

---
+++ layouts/tablet.vue
@@ -0,0 +1,8 @@
+<template>
+    <section class="flex w-full relative">
+        <main class="flex-1 flex">
+            <div class="bg-green-700 h-screen w-1/3 p-4"></div>
+            <slot />
+        </main>
+    </section>
+</template>
