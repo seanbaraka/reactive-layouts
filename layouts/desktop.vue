733e21a3753430e66c62a98dfb3ac6f54be1a96e
seanbaraka <seanbaraka@gmail.com>
2023-06-12 22:28:08 +0300

    feat: desktop layout

---
+++ layouts/desktop.vue
@@ -0,0 +1,10 @@
+<template>
+    <section class="flex w-full relative">
+        <aside class="bg-red-500 h-screen w-1/5 p-4">
+        </aside>
+        <main class="flex-1 flex">
+            <div class="bg-green-700 h-screen w-1/3 p-4"></div>
+            <slot />
+        </main>
+    </section>
+</template>
